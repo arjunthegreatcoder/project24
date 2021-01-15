c170196228eb439413e5e52282d7b0e7054c7033
arjunthegreatcoder <73948843+arjunthegreatcoder@users.noreply.github.com>
2021-01-15 16:06:34 +0530

    Add files via upload

---
+++ project24/MatterJSBoilerPlate-master/paper.js
@@ -0,0 +1,20 @@
+class Paper{
+    constructor(x,y,r) {
+      var options = {
+          restitution : 0, 
+          friction : 0.5,
+          isStatic : false,
+      }
+      this.body = Bodies.circle(x,y,r,options);
+     this.r = r ;
+      World.add(world, this.body);
+    }
+    display(){
+      var pos =this.body.position;
+      push();
+     translate(pos.x,pos.y);
+     ellipseMode(RADIUS);
+     ellipse(0,0,this.r);
+     pop();
+    }
+  };
